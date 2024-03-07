f00f91f223eeea2915596b5e44a80710fc9628f4
Liam Stoldt <liamstoldt@yahoo.com>
2024-03-07 19:26:55 +1030

    chore: ignore update, cargo update, main prep

--- taurus/src-tauri/src/main.rs
+++ taurus/src-tauri/src/main.rs
@@ -1,6 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// local modules
+mod file_management;
+mod finance_computation;
+mod yfinance_interactions;
+
+// crates
+use std::fs;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +16,19 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // check data directories
+    let check_dir = fs::create_dir("./data/");
+    match check_dir {
+        Ok(()) => (),
+        Err(_e) => ()
+    }
+    let check_dir = fs::create_dir("./data/portfolios/");
+    match check_dir {
+        Ok(()) => (),
+        Err(_e) => ()
+    }
+
+    // build tauri
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
